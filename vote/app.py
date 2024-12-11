d78ca13aa39beed39fb5e73cf10848c5d08094fe
frank <frank.chirapa@gmail.com>
2024-12-11 18:39:58 -0500

    initial commit, clean app

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Toronto")
+option_b = os.getenv('OPTION_B', "Sarnia")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
